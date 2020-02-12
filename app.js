ecff93e08b7042faed604341d9dbfb4a39afe1fe
kaiqnes <kaiqnes@gmail.com>
2020-02-12 12:37:49 -0300

    CORS

--- app.js
+++ app.js
@@ -7,20 +7,37 @@ app.use(morgan('dev')) // Vai logar no console todas as entradas
 app.use(bodyParser.urlencoded({ extended: false })) // Aceitará apenas dados simples
 app.use(bodyParser.json()) // Aceitará JSON no body
 
-// rotas específicas
+// TRATAMENTO DE CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*') // Permitindo requisições de todos
+    // res.header('Access-Control-Allow-Origin', 'https://meuServer.com.br') // Permitindo apenas de server específico
+    res.header('Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).send({})
+    }
+
+    next()
+})
+
+// ROTAS ESPECÍFICAS
 const rotaProdutos = require('./routes/produtos')
 const rotaPedidos = require('./routes/pedidos')
 
 app.use('/produtos', rotaProdutos)
 app.use('/pedidos', rotaPedidos)
 
-// TRATAMENTO DE 404
+// MENSAGEM DE ERRO 404
 app.use((req, res, next) => {
     const error = new Error('Não encontrado')
     error.status = 404
     next(error)
 })
 
+// MENSAGEM DE ERRO 500
 app.use((error, req, res, next) => {
     res.status(error.status || 500)
     return res.send({
